a198e885ffe9bbd1cd0fc7da2b489d458660366d
TMDeater <dannyng10231996@gmail.com>
2019-03-24 23:41:38 +0800

    File Relocated
    File Make Up

---
+++ 2ndNodeJsProgram/js/main.js
@@ -0,0 +1,45 @@
+var nodemailer = require('nodemailer');
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  if (req.url == '/postdata'){
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk.toString(); // convert Buffer to string
+    });
+    req.on('end', () => {
+        console.log(body);
+        res.end(body+' had been posted');
+    });
+  } else {
+    fs.readFile('html/index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+}).listen(8080);
+
+// var transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: 'youremail@gmail.com',
+//     pass: 'yourpassword'
+//   }
+// });
+//
+// var mailOptions = {
+//   from: 'youremail@gmail.com',
+//   to: 'myfriend@yahoo.com',
+//   subject: 'Sending Email using Node.js',
+//   text: 'That was easy!'
+// };
+
+// transporter.sendMail(mailOptions, function(error, info){
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('Email sent: ' + info.response);
+//   }
+// });
--- README.md
+++
@@ -1,3 +0,0 @@
-# 1stNodeJSPractice
-This is repository of my first nodeJS practice<br />
-credit from https://www.w3schools.com/nodejs/default.asp
